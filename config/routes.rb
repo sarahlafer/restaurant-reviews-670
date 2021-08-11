fb4b28736a39941820b1f921e33ddd7c2a9c7510
Sarah Lafer <svlafer@gmail.com>
2021-08-11 11:23:57 +0200

    add restaurants and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      # get "/top", to: "restaurants#top"
+      get :top
+    end
+
+    member do
+      get :chef
+      # get "/chef", to: "restaurants#chef"
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# /reviews/new
+
+# /restaurants/:id/reviews/new
+
+
+
+
+# /restaurants/top
+
+
+# collection
+
+
+# member
+
+# /restaurant/2/city
